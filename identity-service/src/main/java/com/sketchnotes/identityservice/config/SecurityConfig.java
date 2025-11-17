beeb0931acf3d2d3d7a4bcd46e91a720bb7fa2a4
Tai Ha <huytainp@gmail.com>
2025-11-17 23:33:33 +0700

    update: public api verify mail

--- identity-service/src/main/java/com/sketchnotes/identityservice/config/SecurityConfig.java
+++ identity-service/src/main/java/com/sketchnotes/identityservice/config/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
             "/api/auth/register",
             "/api/auth/login",
             "/api/blogs",
-            "/api/auth/verify-email",
+            "/api/auth/send-verify-email",
             "/api/auth/refresh-token",
             "/api/auth/login-google",
             "/api/payment/payos/webhook",
